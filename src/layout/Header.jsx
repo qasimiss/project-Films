0f68f00b36e0ebe226c7bee6162660a73058fbd0
German Vengerov <qasimis@yandex.ru>
2022-10-28 23:31:55 +0400

    add architecture and header layout

---
+++ src/layout/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return( 
+        <nav className="purple darken-4">
+            <div className="nav-wrapper">
+                <a href="#" className="brand-logo">React Movies</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="#">Repository</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
