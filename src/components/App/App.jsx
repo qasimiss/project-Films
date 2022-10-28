ec6af99dc64cb8fb334e9fdbfa45daf2a7d7b529
German Vengerov <qasimis@yandex.ru>
2022-10-28 23:45:09 +0400

    feat:add main

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,14 @@
 import Header from "../../layout/Header"
 import Footer from "../../layout/Footer"
+import Main from "../../layout/Main"
+
 const App = () => {
     return(
         <>
             <Header />
+            <Main />
             <Footer />
+            
         </>
     )
 }
---
+++ src/layout/Main.jsx
@@ -0,0 +1,8 @@
+const Main = () => {
+    return (
+        <main className="container content">
+            <h1> Hi ther!</h1>
+        </main>
+    )
+}
+export default Main
