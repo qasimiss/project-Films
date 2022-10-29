68a1f83a87c3b007538c4960f27e0ad2236d1bf3
German Vengerov <qasimis@yandex.ru>
2022-10-29 20:16:58 +0400

    add links to header and footer

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,12 +1,17 @@
 const Footer = () => {
-    return(
+    return (
         <footer className="page-footer purple darken-2">
-          <div className="footer-copyright">
-            <div className="container">
-            © {new Date().getFullYear()} Copyright Text
-            <a className="grey-text text-lighten-4 right" href="#">Repository</a>
+            <div className="footer-copyright">
+                <div className="container">
+                    © {new Date().getFullYear()} Copyright Text
+                    <a
+                        className="grey-text text-lighten-4 right"
+                        href="https://github.com/qasimiss/project-Films/tree/master"
+                    >
+                        Repository
+                    </a>
+                </div>
             </div>
-          </div>
         </footer>
     )
 }
--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,16 +1,24 @@
 const Header = () => {
-    return( 
+    return (
         <header>
             <nav className="purple darken-4">
                 <div className="nav-wrapper">
-                    <a href="#" className="brand-logo">React Movies</a>
+                    <a
+                        href="https://qasimiss.github.io/project-Films/"
+                        className="brand-logo"
+                    >
+                        React Movies
+                    </a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><a href="#">Repository</a></li>
+                        <li>
+                            <a href="https://github.com/qasimiss/project-Films/tree/master">
+                                Repository
+                            </a>
+                        </li>
                     </ul>
                 </div>
             </nav>
         </header>
-        
     )
 }
 
